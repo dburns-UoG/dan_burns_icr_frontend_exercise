1f44a2ec3bc5a3df34d272ca04eddab77cb48974
dburns-UoG <49986477+dburns-UoG@users.noreply.github.com>
2019-08-21 11:48:16 +0100

    Committing build for github pages

---
+++ build/precache-manifest.f05250027f5868ca07d1c01e04c37ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43226a4c1bcfff9b3e6665233b9bb8f",
+    "url": "/dan_burns_icr_frontend_exercise/index.html"
+  },
+  {
+    "revision": "5cd80feda3453e1f6f6e",
+    "url": "/dan_burns_icr_frontend_exercise/static/css/main.f348d377.chunk.css"
+  },
+  {
+    "revision": "652492be0f30a5d2d089",
+    "url": "/dan_burns_icr_frontend_exercise/static/js/2.e28f807c.chunk.js"
+  },
+  {
+    "revision": "5cd80feda3453e1f6f6e",
+    "url": "/dan_burns_icr_frontend_exercise/static/js/main.a8c7f342.chunk.js"
+  },
+  {
+    "revision": "66b002ef8af57c1870e2",
+    "url": "/dan_burns_icr_frontend_exercise/static/js/runtime~main.08c0c822.js"
+  }
+]);
